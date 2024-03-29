035bb969e53b6f2f07944636c2ceff6e9a8c7b34
KamiliaNurmala <kamilia.nurmala.ft23@mail.umy.ac.id>
2024-03-29 10:26:01 +0700

    membuat if statement untuk menanyakan keberlanjutan progam

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -86,4 +86,14 @@ void display() {
 
 int main() {
 	char ch;
+	do {
+		input();
+		q_short(0, n - 1);
+		display();
+		cout << "\n\nDo you want to continue? (y/n): ";
+		cin >> ch;
+		if (ch == 'n' || ch == 'N')
+			break;
+		system("cls");
+	}
 }
