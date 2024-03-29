ad010dd31c94ad7788fb14fba5f833bd0e638963
KamiliaNurmala <kamilia.nurmala.ft23@mail.umy.ac.id>
2024-03-29 10:21:18 +0700

    membuat for loop untuk mendisplay hasilnya

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -70,3 +70,13 @@ void q_short(int low, int high) {
 		q_short(j + 1, high);
 	}
 }
+
+void display() {
+	cout << "\n--------------------" << endl;
+	cout << "Sorted Array" << endl;
+	cout << "--------------------" << endl;
+
+	for (int i = 0; i < n; i++) {
+		cout << arr[i] << " ";
+	}
+}
